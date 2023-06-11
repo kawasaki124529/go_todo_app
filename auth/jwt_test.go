bb18af73b48480df7de3091364729b4dd27650ce
kawasaki <yuko124529@yahoo.co.jp>
2023-06-11 16:13:04 +0900

    add getToken test

--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -3,11 +3,20 @@ package auth
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/kawasaki124529/go_todo_app/clock"
 	"github.com/kawasaki124529/go_todo_app/entity"
 	"github.com/kawasaki124529/go_todo_app/testutil/fixture"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 )
 
 func TestEmbed(t *testing.T) {
@@ -42,5 +51,56 @@ func TestJWTer_GenerateToken(t *testing.T) {
 	}
 	if len(got) == 0 {
 		t.Error("want token, but got empty")
+	}
+}
+
+func TestJWTer_GetToken(t *testing.T) {
+	t.Parallel()
+
+	c := clock.FixedClocker{}
+	want, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer(`github.com/kawasaki124529/go_todo_app`).
+		Subject("access_token").
+		IssuedAt(c.Now()).
+		Expiration(c.Now().Add(30*time.Minute)).
+		Claim(RoleKey, "test").
+		Claim(UserNameKey, "test_user").
+		Build()
+	if err != nil {
+		t.Fatal(err)
+	}
+	pkey, err := jwk.ParseKey(rawPrivKey, jwk.WithPEM(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+	signed, err := jwt.Sign(want, jwt.WithKey(jwa.RS256, pkey))
+	if err != nil {
+		t.Fatal(err)
+	}
+	userID := entity.UserID(20)
+
+	ctx := context.Background()
+	moq := &StoreMock{}
+	moq.LoadFunc = func(ctx context.Context, key string) (entity.UserID, error) {
+		return userID, nil
+	}
+	sut, err := NewJWTer(moq, c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest(
+		http.MethodGet,
+		`https://github.com/kawasaki124529`,
+		nil,
+	)
+	req.Header.Set(`Authorization`, fmt.Sprintf(`Bearer %s`, signed))
+	got, err := sut.GetToken(ctx, req)
+	if err != nil {
+		t.Fatalf("want no error, but got %v", err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("GetToken() got = %v, want %v", got, want)
 	}
 }
